50f6b7d575c68a9f2cb6b2a9b4630585637422f3
Yaswanth <yryr2004@gmail.com>
2024-06-20 21:06:34 +0530

    Users can now be displayed

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.routes.js";
 import establishMongoConnection from "./db/establishMongoConnection.js";
 import messageRoutes from "./routes/message.routes.js";
 import cookieParser from "cookie-parser";
+import userRoute from "./routes/user.routes.js";
 
 const app = express();
 const port = process.env.port || 5000;
@@ -14,6 +15,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoute);
 
 app.listen(port, () => {
   establishMongoConnection();
