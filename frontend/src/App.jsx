93df1c25d347cff79d7d55e938ad150626d93ee3
Yaswanth <yryr2004@gmail.com>
2024-06-25 14:24:29 +0530

    Connected Signup page with backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,18 @@ import "./App.css";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/Signup";
 import Home from "./pages/home/Home";
+import { Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      {/* <Login /> */}
-      {/* <SignUp /> */}
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+      </Routes>
+      <Toaster />
     </div>
   );
 }
