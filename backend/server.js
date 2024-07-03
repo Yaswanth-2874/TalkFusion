1084981e68eefb130454c7c15cbcf6e2ea127acd
Yaswanth <yryr2004@gmail.com>
2024-07-03 14:49:09 +0530

    Ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,10 @@ import messageRoutes from "./routes/message.routes.js";
 import cookieParser from "cookie-parser";
 import userRoute from "./routes/user.routes.js";
 import { app, server } from "./socket/socket.js";
+import path from "path";
 
 const port = process.env.port || 5000;
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -16,6 +18,11 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoute);
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(port, () => {
   establishMongoConnection();
